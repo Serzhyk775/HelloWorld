986429ba020758c04068590e1ecc4f4748d32bfa
serzhyk775 <s.a.vostyan@mail.ru>
2021-01-21 22:04:14 +0300

    second

---
+++ src/eu/senla/com/Global.java
@@ -0,0 +1,23 @@
+package eu.senla.com;
+
+public class Global {
+    int A=3;
+    double B=4;
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int a) {
+        A = a;
+    }
+
+    public double getB() {
+        return B;
+    }
+
+    public void setB(double b) {
+        B = b;
+    }
+}
+/*пошел каким то сложным путем, надо еще время поразбираться */
---
+++ src/eu/senla/com/Main.java
@@ -0,0 +1,10 @@
+package eu.senla.com;
+
+public class Main {
+    public static void main(String[] args) {
+int A=3;
+double B=5;
+   A=(int)B;
+        System.out.println("A="+B);
+    }
+}
