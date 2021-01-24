d2a1e0a62d60d6862653c21a6d355377fa6ab366
serzhyk775 <s.a.vostyan@mail.ru>
2021-01-24 13:33:32 +0300

    third

--- src/eu/senla/com/Main.java
+++ src/eu/senla/com/Main.java
@@ -1,10 +1,13 @@
 package eu.senla.com;
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
-int A=3;
-double B=5;
-   A=(int)B;
-        System.out.println("A="+B);
+
+    int A=3;
+    double B=5;
+    A=(int)B;
+
+    System.out.println("A="+B);
+
     }
 }
